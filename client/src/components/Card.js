79de85b9ade7906d717ab1db9a3a5299c371520b
sarahlcox <sarahl.cox.22@gmail.com>
2020-12-01 18:05:43 -0600

    Got cards to appear for carousel

---
+++ client/src/components/Card.js
@@ -0,0 +1,11 @@
+// this is the component for the cards in the carousel items 
+import React from 'react';
+import CardInfo from '../components/CardInfo';
+
+function Card(props){
+    return(
+        <p>Card Works!</p>
+    )
+}
+
+export default Card;
--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -6,7 +6,7 @@ import WebDev from '../assets/images/WebDev.jpg';
 import About from '../assets/images/About.jpg';
 import Photography from '../assets/images/Photography.jpg';
 
-
+// create the carousel component and items that contain images 
 class Carousel extends React.Component{
     constructor(props) {
         super(props);
@@ -33,17 +33,44 @@ class Carousel extends React.Component{
                 title: 'Photography',
                 subTitle: 'View My Photography Website',
                 imgSrc: Photography,
-                link: 'https://featherprintphotography.com',
+                link: 'https://featherprint',
                 selected: false
             },
         ]}
     }
+
+    handleCardClick = (id, card) => {
+        // spread operator for the array items to display
+        let items = [...this.state.items];
+        // when the item is selected set it to false/toggle, if not selected set it to true 
+        items[id].selected = items[id].selected ? false : true;
+        // loop through each one and change the ones not selected is true
+        items.forEach(item => {
+            if(item.id !== id) {
+                item.selected= false;
+            }
+        });
+        // set the state for the array
+        this.setState({
+            items
+        });
+    }
+    // instead of using render create a method that maps through the items and makes a hard component 
+    makeItems= (items) =>{
+        return items.map(item => {
+        return <Card item={item} click={(e =>this.handleCardClick(item.id, e))} key={item.id} />
+        })
+    }
+    // using bootstrap elements to wrap the items nicely
     render() {
         return(
-            <p>Carousel Works!</p>
+            <Container fluid={true}>
+                <Row className="justify-content-around">
+                    {this.makeItems(this.state.items)}
+                </Row>
+            </Container>
         );
     }
+
 }
-
-
 export default Carousel;
