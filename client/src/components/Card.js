317d14a0aab953d7479922387e72675fcf6e988a
sarahlcox <sarahl.cox.22@gmail.com>
2020-12-01 18:08:31 -0600

    got images on cards

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -4,8 +4,11 @@ import CardInfo from '../components/CardInfo';
 
 function Card(props){
     return(
-        <p>Card Works!</p>
-    )
+            <div className="d-inline-block g-card" onClick={(e) => props.click(props.item)} >
+                <img className="g-card-image" src={props.item.imgSrc} alt={props.item.imgSrc} />
+                {props.item.selected && <CardInfo title={props.item.title} subTitle={props.item.subTitle} link={props.item.link} />}
+            </div>
+        );
 }
 
 export default Card;
