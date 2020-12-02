f9f38d90e79eb41194364a3ce5a64fb1ce2ebd99
sarahlcox <sarahl.cox.22@gmail.com>
2020-12-01 18:40:37 -0600

    added about me info page

--- client/src/pages/AboutPage.js
+++ client/src/pages/AboutPage.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import Hero from '../components/Hero';
+import Content from '../components/Content';
 
-function AboutPage(props){
-    return (
-    <p>AboutPage Works!</p>
+function AboutPage(props) {
+
+    return(
+        <div>
+            <Hero title={props.title} />
+
+            <Content>
+                <p>this is my about me page</p>
+            </Content>
+        </div>
     );
+
 }
+
 export default AboutPage;
